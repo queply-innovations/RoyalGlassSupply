1313342984ea46e3965caf1c436b8b59fc6e621f
gabby <gabbypaylaga@gmail.com>
2023-12-05 05:15:09 +0800

    refactor: page-title and child

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -3,16 +3,32 @@ import Sidebar from '@/components/Sidebar/Sidebar';
 import React, { ReactNode } from 'react';
 
 interface MainLayoutProps {
+	pageTitle?: string;
 	children: ReactNode;
+	cn?: string;
 }
 
-const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
+const MainLayout: React.FC<MainLayoutProps> = ({ children, pageTitle, cn }) => {
+	const toTitleSelector = pageTitle?.toLowerCase().replace(' ', '-');
 	return (
 		<div className="main-layout flex w-screen flex-row bg-primary-background">
 			<Sidebar />
 			<div className="main-layout-content flex h-screen w-full flex-col gap-y-3 p-5">
 				<Navbar />
-				<div className="main-layout-children flex-1">{children}</div>
+				<div className="main-layout-children flex-1">
+					<div
+						className={`${toTitleSelector}-layout flex h-full flex-col gap-y-4`}
+					>
+						<h1 className="page-title text-3xl font-bold capitalize text-primary-dark-gray">
+							{pageTitle}
+						</h1>
+						<div
+							className={`${toTitleSelector}-container flex h-full flex-col gap-3 ${cn}`}
+						>
+							{children}
+						</div>
+					</div>
+				</div>
 				<div className="main-layout-footer flex items-end justify-end">
 					<p className="text-xs text-primary-dark-gray">You are online</p>
 				</div>
