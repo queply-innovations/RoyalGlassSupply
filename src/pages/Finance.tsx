7f28a94113c711f96f314e5419b30eb954938149
gabby <gabbypaylaga@gmail.com>
2023-12-05 07:24:33 +0800

    style: spacing between searchbar and select option

--- src/pages/Finance.tsx
+++ src/pages/Finance.tsx
@@ -10,9 +10,9 @@ const Finance = () => {
 			pageTitle="Finance"
 			cn="rounded-lg border border-black/10 bg-white p-5"
 		>
-			<div className="finance-header flex gap-x-10">
+			<div className="finance-header flex justify-between gap-x-10">
 				<Searchbar ButtonIcon={false} />
-				<div className="selectbox-container flex flex-row gap-2 rounded border border-black/10 bg-primary-white p-3">
+				<div className="selectbox-container flex flex-row gap-2 rounded border border-black/10 bg-primary-white p-3 ">
 					<SelectBox
 						options={financeOption}
 						hasFrequency={true}
