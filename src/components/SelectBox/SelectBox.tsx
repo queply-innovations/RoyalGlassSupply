eb5bc9963c8621fb279e3fd07fddde1cef9a6265
gabby <gabbypaylaga@gmail.com>
2023-12-05 07:13:48 +0800

    test: added selectbox and finance page

---
+++ src/components/SelectBox/SelectBox.tsx
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+
+interface SelectBoxProps {
+	options: string[];
+	hasFrequency: boolean;
+	frequencyOptions?: string[];
+}
+
+const SelectBox: React.FC<SelectBoxProps> = ({
+	options,
+	hasFrequency,
+	frequencyOptions,
+}) => {
+	const [selectedValue, setSelectedValue] = useState<string | undefined>(
+		undefined,
+	);
+
+	const [selectedFrequency, setSelectedFrequency] = useState<
+		string | undefined
+	>(undefined);
+
+	const handleOptionChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+		const selectedOption = event.target.value;
+		setSelectedValue(selectedOption);
+		console.log(`Selected Value: ${selectedOption}`);
+	};
+
+	const handleFrequencyChange = (
+		event: React.ChangeEvent<HTMLSelectElement>,
+	) => {
+		const selectedFrequencyOption = event.target.value;
+		setSelectedFrequency(selectedFrequencyOption);
+		console.log(`Selected Value: ${selectedFrequencyOption}`);
+	};
+
+	return (
+		<>
+			<div className="selectbox-container ">
+				<select
+					className="flex min-w-[10rem] rounded border border-black/10 bg-white p-2 font-semibold"
+					id="selectBox"
+					value={selectedValue}
+					onChange={handleOptionChange}
+				>
+					<option value="" disabled selected hidden>
+						Sort By
+					</option>
+					{options.map((option, index) => (
+						<option
+							className="cursor-pointer rounded-none border-none hover:bg-primary-red"
+							key={index}
+							value={option}
+						>
+							{option}
+						</option>
+					))}
+				</select>
+			</div>
+			{hasFrequency && (
+				<div className="frequency-container">
+					<select
+						className="flex rounded border border-black/10 bg-white p-2 font-semibold"
+						id="frequencyBox"
+						value={selectedFrequency}
+						onChange={handleFrequencyChange}
+					>
+						<option value="" disabled hidden selected>
+							Sort By
+						</option>
+						{frequencyOptions &&
+							frequencyOptions.map((freq, index) => (
+								<option key={index} value={freq}>
+									{freq}
+								</option>
+							))}
+					</select>
+				</div>
+			)}
+		</>
+	);
+};
+
+export default SelectBox;
--- src/pages/Finance.tsx
+++ src/pages/Finance.tsx
@@ -1,16 +1,26 @@
 import Searchbar from '@/components/Navbar/Searchbar';
+import SelectBox from '@/components/SelectBox/SelectBox';
 import MainLayout from '@/layouts/MainLayout';
-import React from 'react';
 
 const Finance = () => {
+	const financeOption = ['Transaction', 'Transfer'];
+	const financeFrequency = ['Daily', 'Weekly', 'Monthly', 'Yearly'];
 	return (
-		<MainLayout pageTitle="Finance">
-			<div className="finance-container flex h-full flex-col gap-3 rounded-lg border border-black/10 bg-white p-5">
-				<div className="finance-header flex gap-x-10">
-					<Searchbar ButtonIcon={false} />
+		<MainLayout
+			pageTitle="Finance"
+			cn="rounded-lg border border-black/10 bg-white p-5"
+		>
+			<div className="finance-header flex gap-x-10">
+				<Searchbar ButtonIcon={false} />
+				<div className="selectbox-container flex flex-row gap-2 rounded border border-black/10 bg-primary-white p-3">
+					<SelectBox
+						options={financeOption}
+						hasFrequency={true}
+						frequencyOptions={financeFrequency}
+					/>
 				</div>
-				<div className="finance-table-container flex h-full w-full rounded-md"></div>
 			</div>
+			<div className="finance-table-container flex h-full w-full rounded-md"></div>
 		</MainLayout>
 	);
 };
