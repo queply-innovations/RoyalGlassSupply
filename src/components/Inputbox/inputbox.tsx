2d5e1c62550106e52f07b69599f1740274d89192
gabby <gabbypaylaga@gmail.com>
2023-12-05 16:18:50 +0800

    style: change to lowercase

--- src/components/Inputbox/inputbox.tsx
+++ src/components/Inputbox/inputbox.tsx
@@ -1,20 +1,28 @@
 import React from 'react';
 
 interface InputboxProps {
+	label?: string;
 	value?: string;
 	placeholder: string;
 }
 
-export const Inputbox: React.FC<InputboxProps> = ({ value, placeholder }) => {
+export const Inputbox: React.FC<InputboxProps> = ({
+	value,
+	placeholder,
+	label,
+}) => {
 	const [newValue, SetNewValue] = React.useState('');
 	const onChange = (e: React.FormEvent<HTMLInputElement>) => {
 		SetNewValue(e.currentTarget.value);
 	};
 	return (
 		<>
-			<div className="InputboxContainer">
+			<div className="inputbox-container flex flex-col gap-2">
+				<label className="inputbox-label font-Inter text-base font-semibold uppercase text-black">
+					{label}
+				</label>
 				<input
-					className="InputboxContent"
+					className="inputbox-input rounded-full border border-black/10 bg-primary-white px-3 py-1 font-Inter text-base font-semibold outline-primary-dark-gray focus:outline"
 					type="text"
 					value={value}
 					placeholder={placeholder}
